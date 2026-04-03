b3fb495240ff6236ce4fa733b97e90bb7384e845
luvya <luvya1411@gmail.com>
2026-04-03 12:40:04 +0530

    errors fixed

---
+++ index.js
@@ -0,0 +1,107 @@
+var titles = ['','','','','','','','',''];
+var turn = 'X' ;
+var going = true;
+
+
+var lines = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+];
+
+var area = document.getElementById('box1');
+var txt = document.getElementById('info');
+
+
+
+function setup(){
+    area.innerHTML = '';
+    for(var i=0; i<9; i++){
+        var b = document.createElement('button');
+        b.className = 'cell';
+           b.setAttribute('data-i',i);
+        b.onclick= picked ;
+        area.appendChild(b);
+
+
+    }
+}
+
+
+function picked(){
+    var i = this.getAttribute('data-i');
+    if(!going) return;
+    if(titles[i]!='') return;
+
+
+
+    titles[i] = turn;
+    this.innerHTML = turn;
+
+    var w = won();
+    if(w){
+        txt.textContent = 'Player ' + w + ' wins!';
+        going=false;
+        return;
+
+    }
+
+
+
+
+    var full = true;
+    for(var j = 0; j<titles.length; j++){
+        if(titles[j]=='') { full=false; break; }
+    }
+
+
+    if(full){
+        txt.textContent = 'Draw!';
+        going = false; 
+        return;
+
+    }
+
+
+    turn = turn=='X' ? 'O' : 'X';
+  txt.textContent = 'Player ' + turn + "'s turn";
+}
+
+
+
+
+function won(){
+    for (var k=0; k<lines.length; k++){
+        var a = lines[k][0], b=lines[k][1], c=lines[k][2];
+        if(titles[a]!='' && titles[a]==titles[b] && titles[b]==titles[c]){
+          return titles[a];
+        }
+    }
+    return null;
+
+}
+
+
+document.getElementById('again').onclick = function(){
+    titles = ['','','','','','','','',''];
+    turn = 'X';
+    going = true;
+    txt.textContent = 'Player X goes first';
+    setup();
+}
+
+setup();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
